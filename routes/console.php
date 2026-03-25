84103c7c13fba9268fcd2a3365ce7b9e51426ae5
Ivan Andonov <ivan@ttc.media>
2026-03-25 12:52:18 +0700

    wip

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('bot:poll')->everyThirtySeconds()->withoutOverlapping();
-Schedule::command('bot:update-prices')->everyThirtySeconds()->withoutOverlapping();
+Schedule::command('bot:poll')->everyThirtySeconds()->withoutOverlapping(5);
+Schedule::command('bot:update-prices')->everyThirtySeconds()->withoutOverlapping(5);
 Schedule::command('bot:check-resolved')->everyFiveMinutes()->withoutOverlapping();
 Schedule::command('bot:check-wallets')->everyFiveMinutes()->withoutOverlapping();
 Schedule::command('bot:discover-wallets')->hourly()->withoutOverlapping();
